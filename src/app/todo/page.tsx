c471d39df11a81d7e0de43302caff18bb2382382
Nakawama keiya <c1208034@st.kanazawa-it.ac.jp>
2024-02-07 10:48:45 +0900

    TODOリスト拡張機能

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react"
 import Button from "../components/Button"
-import {List,ListItem,ListIcon,OrderedList,UnorderedList,} from '@chakra-ui/react'
+import {List,ListItem,ListIcon,OrderedList,UnorderedList,Checkbox, CheckboxGroup, Stack} from '@chakra-ui/react'
 
 
 const TODO = () => {
@@ -18,19 +18,39 @@ const TODO = () => {
         setTodos([...todos,value])
     }
 
+    const handleLiset = () => {
+        setTodos([])
+    }
+
     return(
         <div className="h-screan flex flex-col justify-center items-center gap-4">
+
             TODO
-            <div>
+            <div className="flex flex-row justify-center items-center gap-4">  
                 <input value={value} onChange={handleChange}/>
+                
+                <div className="flex flex-col">
+
                 <Button onClick={handleClick}>
-                追加
+                    追加
                 </Button>
+                <Button onClick={handleLiset}>
+                    リセット
+                </Button>
+                </div>
+                
+                
             </div>
             <div>
                 <UnorderedList>
                     {todos.map((todo) => (
-                        <ListItem key={todo}>{todo}</ListItem>
+                        <ListItem key={todo}>
+                            <div className="flex flex-col">
+                            <Stack spacing={5} direction='column'>
+                                <Checkbox isInvalid>{todo}</Checkbox>
+                            </Stack>
+                            </div>
+                        </ListItem>
                     ))}
                 </UnorderedList>
             </div>
